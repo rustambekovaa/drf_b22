24a881285043df44528fde9cde1c2a2e59d5163c
Akmaanai <rustambekovaa15@gmail.com>
2026-09-23 16:12:24 +0600

    ui

--- core/settings.py
+++ core/settings.py
@@ -142,3 +142,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+}
